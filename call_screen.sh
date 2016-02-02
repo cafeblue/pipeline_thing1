192efca368123d49b5c4a90cbeab7a457293f22e
Wei Wang <periwinkle.david@gmail.com>
2016-02-02 18:28:54 -0500

    change the hard code file location to soft code

--- call_screen.sh
+++ call_screen.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 today=$(date '+%Y%m%d')
-CMD="/home/wei.wang/pipeline_development/call_pipeline.pl "$@" >>/home/wei.wang/pipeline_log/pl_resubmit.log 2>>/home/wei.wang/pipeline_log/pl_resubmit.err"
+CMD="$@ >>/home/wei.wang/pipeline_log/pl_resubmit.log 2>>/home/wei.wang/pipeline_log/pl_resubmit.err"
 screen -D -r weiscr${today} -p 0 -X stuff "$CMD$(printf \\r)"
