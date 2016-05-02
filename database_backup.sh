cd98f3f0bf18d14f80bcf8ceb8ee3eabca866513
Wei Wang <weiw.wang@sickkids.ca>
2016-05-02 17:20:44 -0400

    fix a typo

--- database_backup.sh
+++ database_backup.sh
@@ -4,4 +4,4 @@ timestamp=$(date +"%m-%d-%y");
 type=$1;
 
 ##backups the mysql database clinicalA
-/database/infobright/iee-4.8.2/infobright/bin/mysqldump -u llau -pjelly9belly clinicalA  --single-transaction | gzip > /localhd/db_backupi_v5/${type}/clinicalA.${timestamp}.mysql.gz
+/database/infobright/iee-4.8.2/infobright/bin/mysqldump -u llau -pjelly9belly clinicalA  --single-transaction | gzip > /localhd/db_backup_v5/${type}/clinicalA.${timestamp}.mysql.gz
