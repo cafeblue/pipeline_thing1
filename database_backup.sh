fc9dc260bacd2c60427647bd15586209f6815c66
Wei Wang <weiw.wang@sickkids.ca>
2016-05-02 17:17:55 -0400

    backup files to different folders.

--- database_backup.sh
+++ database_backup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-timestamp=$(date +"%m-%d-%y")
-
-echo "$timestamp"
+timestamp=$(date +"%m-%d-%y");
+type=$1;
 
 ##backups the mysql database clinicalA
-/database/infobright/iee-4.8.2/infobright/bin/mysqldump -u llau -pjelly9belly clinicalA  --single-transaction | gzip > /localhd/db_backup/clinicalA.$timestamp.mysql.gz
+/database/infobright/iee-4.8.2/infobright/bin/mysqldump -u llau -pjelly9belly clinicalA  --single-transaction | gzip > /localhd/db_backupi_v5/${type}/clinicalA.${timestamp}.mysql.gz
