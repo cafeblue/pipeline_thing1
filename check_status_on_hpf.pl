db19aafdac1ec726fcabf838233a325167e548bb
Wei Wang <weiw.wang@sickkids.ca>
2016-05-17 23:08:05 -0400

    Check the post status of pipeline.

--- check_status_on_hpf.pl
+++ check_status_on_hpf.pl
@@ -44,7 +44,7 @@ foreach my $idpair (@$idpair_ref) {
 
     # All jobs finished successfully
     if (&check_all_jobs(@$idpair) == 1) {
-        my $update_CS = "UPDATE sampleInfo set currentStatus = '4', analysisFinishedTime = NOW() where sampleID = '$$idpair[0]' and postprocID = '$$idpair[1]'";
+        my $update_CS = "UPDATE sampleInfo set currentStatus = '4', analysisFinishedTime = NOW(), displayed_at = NOW() where sampleID = '$$idpair[0]' and postprocID = '$$idpair[1]'";
         print $update_CS,"\n";
         my $sthQNS = $dbh->prepare($update_CS) or die "Can't query database for running samples: ". $dbh->errstr() . "\n";
         $sthQNS->execute() or die "Can't execute query for running samples: " . $dbh->errstr() . "\n";
@@ -297,7 +297,7 @@ sub update_jobStatus {
                     my $msg = "jobName " . $joblst[$i] . " for sampleID $sampleID postprocID $postprocID failed with exitcode $1\n";
                     print STDERR $msg;
                     email_error($msg);
-                    $update_query = "UPDATE sampleInfo set currentStatus = '5', analysisFinishedTime = NOW() WHERE sampleID = '$sampleID' AND postprocID = '$postprocID'";
+                    $update_query = "UPDATE sampleInfo set currentStatus = '5', analysisFinishedTime = NOW(), displayed_at = NOW() WHERE sampleID = '$sampleID' AND postprocID = '$postprocID'";
                     $sthUQ = $dbh->prepare($update_query)  or die "Can't query database for running samples: ". $dbh->errstr() . "\n";
                     $sthUQ->execute() or die "Can't execute query for running samples: " . $dbh->errstr() . "\n";
                     return;
