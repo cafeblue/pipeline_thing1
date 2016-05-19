49b137af92ce6e9c467dafb2981100b034c191c6
Wei Wang <periwinkle.david@gmail.com>
2016-05-19 11:25:53 -0400

    update backup folder for newGP.

--- mkdir4newGP.sh
+++ mkdir4newGP.sh
@@ -11,10 +11,10 @@ mkdir ${runfolder}/gatkQscoreRecalibration;
 mkdir ${runfolder}/gatkFilteredRecalVariant;
 mkdir ${runfolder}/windowBed;
 
-ln /hpf/largeprojects/pray/llau/clinical/backup_files/bam/${sampleid}.${oldaid}.realigned-recalibrated.bam ${runfolder}/gatkQscoreRecalibration/${sampleid}.${newaid}.realigned-recalibrated.bam ;
-ln /hpf/largeprojects/pray/llau/clinical/backup_files/bam/${sampleid}.${oldaid}.realigned-recalibrated.bai ${runfolder}/gatkQscoreRecalibration/${sampleid}.${newaid}.realigned-recalibrated.bai ;
-ln /hpf/largeprojects/pray/llau/clinical/backup_files/bam/${sampleid}.${oldaid}.realigned-recalibrated.bam.bai ${runfolder}/gatkQscoreRecalibration/${sampleid}.${newaid}.realigned-recalibrated.bam.bai ;
-ln /hpf/largeprojects/pray/llau/clinical/backup_files/region_vcf/${sampleid}.${oldaid}.${oldgp}.gatk.snp.indel.vcf ${runfolder}/gatkFilteredRecalVariant/${sampleid}.${newaid}.gatk.snp.indel.vcf;
-ln /hpf/largeprojects/pray/llau/clinical/backup_files/region_vcf/${sampleid}.${oldaid}.${oldgp}.gatk.snp.indel.vcf.idx ${runfolder}/gatkFilteredRecalVariant/${sampleid}.${newaid}.gatk.snp.indel.vcf.idx;
-ln /hpf/largeprojects/pray/llau/clinical/backup_files/windowBed/${sampleid}.${oldaid}.hgmd.indel_window20bp.snp_window3bp.tsv ${runfolder}/windowBed/${sampleid}.${newaid}.hgmd.indel_window20bp.snp_window3bp.tsv;
-ln /hpf/largeprojects/pray/llau/clinical/backup_files/windowBed/${sampleid}.${oldaid}.clinvar.window20bp.tsv ${runfolder}/windowBed/${sampleid}.${newaid}.clinvar.window20bp.tsv ;
+ln /hpf/largeprojects/pray/clinical/backup_files_v5/bam/${sampleid}.${oldaid}.realigned-recalibrated.bam ${runfolder}/gatkQscoreRecalibration/${sampleid}.${newaid}.realigned-recalibrated.bam ;
+ln /hpf/largeprojects/pray/clinical/backup_files_v5/bam/${sampleid}.${oldaid}.realigned-recalibrated.bai ${runfolder}/gatkQscoreRecalibration/${sampleid}.${newaid}.realigned-recalibrated.bai ;
+ln /hpf/largeprojects/pray/clinical/backup_files_v5/bam/${sampleid}.${oldaid}.realigned-recalibrated.bam.bai ${runfolder}/gatkQscoreRecalibration/${sampleid}.${newaid}.realigned-recalibrated.bam.bai ;
+ln /hpf/largeprojects/pray/clinical/backup_files_v5/region_vcf/${sampleid}.${oldaid}.${oldgp}.gatk.snp.indel.vcf ${runfolder}/gatkFilteredRecalVariant/${sampleid}.${newaid}.gatk.snp.indel.vcf;
+ln /hpf/largeprojects/pray/clinical/backup_files_v5/region_vcf/${sampleid}.${oldaid}.${oldgp}.gatk.snp.indel.vcf.idx ${runfolder}/gatkFilteredRecalVariant/${sampleid}.${newaid}.gatk.snp.indel.vcf.idx;
+ln /hpf/largeprojects/pray/clinical/backup_files_v5/windowBed/${sampleid}.${oldaid}.hgmd.indel_window20bp.snp_window3bp.tsv ${runfolder}/windowBed/${sampleid}.${newaid}.hgmd.indel_window20bp.snp_window3bp.tsv;
+ln /hpf/largeprojects/pray/clinical/backup_files_v5/windowBed/${sampleid}.${oldaid}.clinvar.window20bp.tsv ${runfolder}/windowBed/${sampleid}.${newaid}.clinvar.window20bp.tsv ;
