bc356a774d6c70e67f98aad4c2d71d3757b0916c
Lynette Lau <lynette.lau@sickkids.ca>
2016-06-20 13:53:37 -0400

    syntax error on offtarget_chr.pl

--- offtarget_chr.pl
+++ offtarget_chr.pl
@@ -11,12 +11,17 @@ my $targetFile = "";
 if ($mgenepanel eq "hiseq") {
   $targetFile = "/hpf/largeprojects/pray/wei.wang/misc_files/target_chr1.bed";
 } else {
+  $targetFile = $mgenepanel;
   $targetFile =~s/exon_10bp_padding.bed/exon_10bp_padding.chr1.bed/;
 }
+print STDERR "targetFile=$targetFile\n";
 my $overall = `samtools view -c $bamfile 1`;
 my $ontarget = `samtools view -c -L $targetFile $bamfile`;
+print STDERR "overall=$overall\n";
+print STDERR "ontarget=$ontarget\n";
 chomp($overall);
 chomp($ontarget);
 
 my $ratio = sprintf('%5.2f', ($overall-$ontarget)*100/$overall);
+
 print "UPDATE sampleInfo SET offTargetRatioChr1 = '$ratio' WHERE sampleID = '$sampleID' AND postprocID = '$postprocID';\n";
