24127e019a439f18cb8afb325b9fdba67f3e055f
Wei Wang <weiw.wang@sickkids.ca>
2016-09-05 12:31:01 -0400

    bug fix for dbh.

--- copy_cancer_sample.pl
+++ copy_cancer_sample.pl
@@ -3,7 +3,7 @@
 use strict;
 use DBI;
 
-my $dbhC = DBI->connect("DBI:mysql:clinicalC;mysql_local_infile=1;host=127.0.0.1;port=5029", wei.wang, baccaharis, { RaiseError => 1 } ) or die ( "Couldn't connect to database: " . DBI->errstr );
+my $dbhC = DBI->connect("DBI:mysql:clinicalC;mysql_local_infile=1;host=127.0.0.1;port=5029", "wei.wang", "baccaharis", { RaiseError => 1 } ) or die ( "Couldn't connect to database: " . DBI->errstr );
 
 my $copy = $dbhC->prepare("INSERT INTO sampleInfo SELECT * from clinicalA.sampleInfo where TIMESTAMPADD(MINUTE,10,time)>=CURRENT_TIMESTAMP and genePanelVer = 'cancer.gp19'") or die "Can't query database for running samples: ". $dbhC->errstr() . "\n";
 $copy->execute();
