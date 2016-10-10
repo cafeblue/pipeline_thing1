29665d2a43eaca1e47cc43d596b608c5675bdf1e
Wei Wang <periwinkle.david@gmail.com>
2016-10-10 12:39:08 -0400

    fix a logic bug.(copy all the sampleSheet.)

--- copy_cancer_sample.pl
+++ copy_cancer_sample.pl
@@ -6,6 +6,9 @@ use DBI;
 my $dbhC = DBI->connect("DBI:mysql:clinicalC;mysql_local_infile=1;host=127.0.0.1;port=5029", "wei.wang", "baccaharis", { RaiseError => 1 } ) or die ( "Couldn't connect to database: " . DBI->errstr );
 my $dbhA = DBI->connect("DBI:mysql:clinicalA;mysql_local_infile=1;host=127.0.0.1;port=5029", "wei.wang", "baccaharis", { RaiseError => 1 } ) or die ( "Couldn't connect to database: " . DBI->errstr );
 
+$copy = $dbhC->prepare("INSERT INTO sampleSheet SELECT * from clinicalA.sampleSheet where TIMESTAMPADD(MINUTE,10,time)>=CURRENT_TIMESTAMP") or die "Can't query database\n";
+$copy->execute();
+
 my $cNum = $dbhA->prepare("SELECT count(*) from clinicalA.sampleInfo where TIMESTAMPADD(MINUTE,10,time)>=CURRENT_TIMESTAMP and genePanelVer = 'cancer.gp19'") or die "Can't query database for running samples: ". $dbhA->errstr() . "\n"; 
 $cNum->execute();
 my @cNum = $cNum->fetchrow_array;
@@ -16,6 +19,3 @@ $copy->execute();
 
 my $update = $dbhC->prepare("UPDATE sampleInfo SET currentStatus = '0' WHERE TIMESTAMPADD(MINUTE,10,time)>=CURRENT_TIMESTAMP and genePanelVer = 'cancer.gp19'") or die "Can't query database for running samples: ". $dbhC->errstr() . "\n";
 $update->execute();
-
-$copy = $dbhC->prepare("INSERT INTO sampleSheet SELECT * from clinicalA.sampleSheet where TIMESTAMPADD(MINUTE,10,time)>=CURRENT_TIMESTAMP") or die "Can't query database\n";
-$copy->execute();
